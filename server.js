8b8dbcd85a9264a37afc26f513f8a99381f3d578
AsherLecover <asherlec5@gmail.com>
2020-09-10 18:57:47 +0300

    first commit

--- server.js
+++ server.js
@@ -35,20 +35,60 @@
 let http = require("http");
 let fs = require("fs");
 let url = require("url");
-var events = require("events");
-var eventEmitter = new events.EventEmitter();
+let events = require("events");
+let eventEmitter = new events.EventEmitter();
+
+const data = fs.readFileSync(`todo.json`, `utf-8`);
+const dataObj = JSON.parse(data)
+
+let arrJson = [
+  {
+    "task": "0000000000000You can't navigate the microchip without backing up the haptic RSS capacitor!",
+    "done": true
+  },
+  {
+    "task": "11111111111I'll copy the primary SQL hard drive, that should feed the EXE card!",
+    "done": true
+  },
+  {
+    "task": "222222222Try to transmit the COM sensor, maybe it will synthesize the cross-platform card!",
+    "done": true
+  },
+  {
+    "task": "33333333333Use the haptic SSL matrix, then you can connect the optical capacitor!",
+    "done": true
+  },
+  {
+    "task": "444444444444Try to generate the RAM matrix, maybe it will hack the mobile bus!",
+    "done": true
+  }
+]
+
+
+
 http.createServer((req, res) => {
+  const { query, pathname} = url.parse(req.url, true)
     let q = url.parse(req.url, true);
     let filename = "." + q.pathname;
+    const pathname1 = req.url
+    console.log('query: ', query.index);
+    
+
+   
     fs.readFile(filename, (err, data) => {
       if (err) {
         fs.readFile("not-found.html", function (err, data) {
+          console.log('pathname1: ', pathname1);
+          
           res.writeHead(404, { "Content-Type": "text/html" });
           res.write(data);
           return res.end();
         });
       } else {
-        console.log(q.pathname.split(".")[1]);
+    
+        
+
+        
         switch (q.pathname.split(".")[1]) {
           case "html":
             res.writeHead(200, { "Content-Type": "text/html" });
@@ -58,11 +98,45 @@ http.createServer((req, res) => {
             break;
             case "json":
                     res.writeHead(200, { "Content-Type": "application/json",
-                    "Access-Control-Allow-Origin": "*",
-                 });
+                    "Access-Control-Allow-Origin": "*",})
+                      
+
+                    if(query.index != undefined){
+                      arrJson.splice(query.index, 1);
+                      fs.writeFile('todo.json', JSON.stringify(arrJson), function (err) {
+                        if (err) throw err;
+                        console.log('Saved! query.index');
+                      });
+                    }
+                    if(query.add != undefined){
+                      arrJson.push({"task":query.add,"done": true})
+                      fs.writeFile('todo.json', JSON.stringify(arrJson), function (err) {
+                        if (err) throw err;
+                        console.log('Saved!  query.add');
+                      });
+                    }
+
+                    if(query.taskComplitedIndex != undefined && query.taskComplitedBool != undefined){
+                      let myBool = query.taskComplitedBool == "false"? false:true
+                      
+                      arrJson[query.taskComplitedIndex].done =  myBool
+                      fs.writeFile('todo.json', JSON.stringify(arrJson), function (err) {
+                        if (err) throw err;
+                        console.log('Saved!  query.add');
+                      });
+                    }
+
+                    if(query.taskToChangeIndex != undefined && query.taskToChange != undefined){                      
+                      arrJson[query.taskToChangeIndex].task = query.taskToChange 
+                      fs.writeFile('todo.json', JSON.stringify(arrJson), function (err) {
+                        if (err) throw err;
+                      });
+                    }
 
 
-
+                    
+                   
+                    
         }
         res.write(data);
         return res.end();
